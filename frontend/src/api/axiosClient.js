8a4b8fe551fc9828c9ac6ef8999d88ad4103d77b
muhzina98 <muhzinan98@gmail.com>
2025-09-28 19:16:27 -0700

    update deployment

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosClient = axios.create({
-baseURL: import.meta.env.VITE_API_BASE,
+baseURL: import.meta.env.VITE_API_BASE  + "/api",
 headers: {
 'Content-Type': 'application/json',
 },
